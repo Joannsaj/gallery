ff08c43932889c2d068eb849a11ef65a8cd8de92
joansaj <joannsaj@gmail.com>
2020-11-14 22:21:09 +0300

    modified models

--- pics/models.py
+++ pics/models.py
@@ -5,8 +5,8 @@ class Photo(models.Model):
     image = CloudinaryField('image')
     name = models.CharField(max_length =30)
     description = models.TextField()
-    # location = models.ForeignKey(Location)
-    # category
+    location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category)
     def __str__(self):
         return self.name
 
@@ -15,9 +15,14 @@ class Photo(models.Model):
         images = cls.objects.all()
         return images
 
-# class Location(models.Model):
-#     country =  models.CharField(max_length =30)
-#     street =  models.CharField(max_length =30)
+class Location(models.Model):
+    location =  models.CharField(max_length =30)
+    
+    def __str__(self):
+        return self.location
 
-# class Category(models.Model):    
-#     category =  models.CharField(max_length =30)
+class Category(models.Model):    
+    category =  models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.category
