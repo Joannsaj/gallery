7bf09a621cf58ceeaba6380aaa50f693443acae6
joansaj <joannsaj@gmail.com>
2020-11-14 22:03:55 +0300

    added admin

--- pics/models.py
+++ pics/models.py
@@ -5,19 +5,19 @@ class Photo(models.Model):
     image = CloudinaryField('image')
     name = models.CharField(max_length =30)
     description = models.TextField()
-    # location
+    # location = models.ForeignKey(Location)
     # category
     def __str__(self):
-        return self.title
+        return self.name
 
     @classmethod
     def images(cls):
         images = cls.objects.all()
         return images
 
-class Location(models.Model):
-    country =  models.CharField(max_length =30)
-    street =  models.CharField(max_length =30)
+# class Location(models.Model):
+#     country =  models.CharField(max_length =30)
+#     street =  models.CharField(max_length =30)
 
-class Category(models.Model):    
-    category =  models.CharField(max_length =30)
+# class Category(models.Model):    
+#     category =  models.CharField(max_length =30)
