4f55c3527d2dcec6b4a317bf58e4b0fdbd29cfc6
joansaj <joannsaj@gmail.com>
2020-11-14 16:22:41 +0300

    added models

--- pics/models.py
+++ pics/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+class Photo(models.Model):
+    image = CloudinaryField('image')
+    name = models.CharField(max_length =30)
+    description = models.TextField()
+    # location
+    # category
+    def __str__(self):
+        return self.title
+
+    @classmethod
+    def images(cls):
+        images = cls.objects.all()
+        return images
+
+class Location(models.Model):
+    country =  models.CharField(max_length =30)
+    street =  models.CharField(max_length =30)
+
+class Category(models.Model):    
+    category =  models.CharField(max_length =30)
