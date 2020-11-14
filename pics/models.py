3b7f62e0948048955636572e58c34d2fb24112e8
joansaj <joannsaj@gmail.com>
2020-11-14 23:33:16 +0300

    added functions to model

--- pics/models.py
+++ pics/models.py
@@ -1,20 +1,6 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
 
-class Photo(models.Model):
-    image = CloudinaryField('image')
-    name = models.CharField(max_length =30)
-    description = models.TextField()
-    location = models.ForeignKey(Location, on_delete= models.SET_NULL)
-    category = models.ForeignKey(Category, on_delete= models.CASCADE)
-    def __str__(self):
-        return self.name
-
-    @classmethod
-    def images(cls):
-        images = cls.objects.all()
-        return images
-
 class Location(models.Model):
     location =  models.CharField(max_length =30)
     
@@ -26,3 +12,17 @@ class Category(models.Model):
 
     def __str__(self):
         return self.category
+
+class Photo(models.Model):
+    image = CloudinaryField('image')
+    name = models.CharField(max_length =30)
+    description = models.TextField()
+    location = models.ForeignKey(Location, on_delete= models.SET_NULL, null= True)
+    category = models.ForeignKey(Category, on_delete= models.CASCADE, default='')
+    def __str__(self):
+        return self.name
+
+    @classmethod
+    def images(cls):
+        images = cls.objects.all()
+        return images        
