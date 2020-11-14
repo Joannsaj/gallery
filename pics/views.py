dc9bc9e028c8700d156d11e0015b5988ed4dfd00
joansaj <joannsaj@gmail.com>
2020-11-14 17:23:59 +0300

    added display functionality

--- pics/views.py
+++ pics/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Photo
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def display(request):
+    image = Photo.images()
+    return render(request,'pics.html',{"image": image})    
