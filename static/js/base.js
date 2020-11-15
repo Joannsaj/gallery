bb8f649d0d4fd06456ea0231305f6379bd768119
joansaj <joannsaj@gmail.com>
2020-11-15 09:39:59 +0300

    added tooltip styles

--- static/js/base.js
+++ static/js/base.js
@@ -1,14 +1,29 @@
+// function myFunction() {
+/* Get the text field */
+// var copyText = document.getElementById("myInput");
+
+/* Select the text field */
+// copyText.select();
+// copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+/* Copy the text inside the text field */
+// document.execCommand("copy");
+
+/* Alert the copied text */
+// alert("Copied the text: " + copyText.value);
+// }
+
 function myFunction() {
-    /* Get the text field */
     var copyText = document.getElementById("myInput");
-
-    /* Select the text field */
     copyText.select();
-    copyText.setSelectionRange(0, 99999); /*For mobile devices*/
-
-    /* Copy the text inside the text field */
+    copyText.setSelectionRange(0, 99999);
     document.execCommand("copy");
 
-    /* Alert the copied text */
-    alert("Copied the text: " + copyText.value);
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copied: " + copyText.value;
+}
+
+function outFunc() {
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copy to clipboard";
 }
